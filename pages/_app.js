5241346154a82e00390e14568a6561e1a2c02d22
- Manraj Singh <manrajmarok41@gmail.com>
2025-08-06 13:19:20 -0400

    added mongo db

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
-import "@/styles/globals.css";
+import { SWRConfig } from "swr";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootswatch/dist/sketchy/bootstrap.min.css";
+import Layout from "./components/Layout";
+import RouteGuard from "./components/RouteGuard";
+
+
+
+const fetcher = async (...args) => {
+  const response = await fetch(...args);
+  if (!response.ok) {
+    throw new Error(`Request failed with status: ${response.status}`);
+  }
+  return response.json();
+};
+
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <RouteGuard>
+      <SWRConfig value={{ fetcher }}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SWRConfig>
+      </RouteGuard>
+    </>
+  );
 }
