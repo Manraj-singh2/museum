20dbe10ffb5084fe8cd94176b9c89e98908084fe
- Manraj Singh <manrajmarok41@gmail.com>
2025-08-06 13:30:05 -0400

    fixed favourites error

--- pages/favourites.js
+++ pages/favourites.js
@@ -1,12 +1,34 @@
-import Favourites from "./components/favourites";
+import { favouritesAtom } from "@/store";
+import { Card, Col, Row } from "react-bootstrap";
+import { useAtom } from "jotai";
+import ArtworkCard from "./components/ArtworkCard";
 
 export default function Fav() {
-  return (
-    <>
-      <h1 align="center">Favourites</h1>
+  const [favouritesList] = useAtom(favouritesAtom);
+  if (!favouritesList) return null;
 
-        <Favourites />
-
-    </>
-  );
+  if (favouritesList) {
+    return (
+      <>
+        <Row className="gy-4">
+          {favouritesList.length ? (
+            favouritesList.map((art) => {
+              return (
+                <Col lg={3} key={art}>
+                  <ArtworkCard objectId={art} />
+                </Col>
+              );
+            })
+          ) : (
+            <Card>
+              <h4>Nothing Here</h4>
+              <p>Try Adding something to favourites...</p>
+            </Card>
+          )}
+        </Row>
+      </>
+    );
+  } else {
+    <p>Loading</p>;
+  }
 }
